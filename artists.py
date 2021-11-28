cb9f4d98758b8e674b928737e03643adab1a5861
goguemona <goguemona@gmail.com>
2021-11-28 22:30:07 +0100

    recherche aléatoire

--- artists.py
+++ artists.py
@@ -1,14 +1,17 @@
 import albumsartists as al
+import random
 
-artist=input("entrer le nom de votre artiste préféré:")
+# artist=input("entrer le nom de votre artiste préféré:")
 
-al.album(artist)
-
-
-artist_album=input("entrer votre album préféré pour voir ces titres:")
-
-al.title(artist,artist_album)
+# al.album(artist)
 
 
+# artist_album=input("entrer votre album préféré pour voir ces titres:")
 
-    
+# al.title(artist,artist_album)
+
+artist=list(al.Artists.artists["gims"]["ceinture noire"])
+
+print(("\n").join((random.choices(artist,weights=None,cum_weights=None,k=9))))
+
+# print(random.choice(['red','black','white','green','brown']))
