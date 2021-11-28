f21d7f4b860e51d90e08c6a042df207d2f764365
goguemona <goguemona@gmail.com>
2021-11-28 19:32:50 +0100

    artists project

---
+++ albumsartists.py
@@ -0,0 +1,58 @@
+class Albums:
+    # ------------album_ed_sheeran---------------
+    ed_sheeran_equals,ed_sheeran_collaboration,ed_sheeran_divid,ed_sheeran_x=("Tides","Shivers","First Times","Bad Habits","Overspass Graffiti","The Joker And The Queen","Leave Your Life","Collide","2step","Stop The Rain","Love in Slow Motion","Visiting","Sandman","Be Right Now"),("Beautiful People","South of The Border","Cross Me","Take Me Back To London","Best Part of me","I Don't Care","Antisocial","Remember The Name","Feels","Put It All On Me","Nothing On You","I Don't Want Your Money","1000 Nights","Way To Break My Heart","Blow"),("Eraser","Castle on the hill","Dive","Shape of you","Perfet","Galway Girl","Happier","New man","Hearts Don't break around here","what do i know","how would you feel","supermarket flowers"),("one","i'm a mess","sing","don't","nina","photograph","bloodstream","tenerife sea","runaway","the man","thinking out loud","afire love")
+
+    # -----------album_celine_dion----------------------
+
+    celine_dion_courage,celine_dion_encoreunsoir,celine_dion_lovemebacktolife,celine_dion_sansattendre,celine_dion_takingchances=("flying on my own","lovers never die","falling in love again","lying down","courage","imperfections","change my mind","say yes","nobody's watching","the chase","for the lover that i lost","baby","i will be stronger","how did you get here","look at us now","perfect goodbye","best of all","heart of glass","boudaries","the hard way"),("plus qu'ailleurs","l'étoile","ma faille","encore un soir","je nous veux","les yeux au ciel","si c'était à refaire","ordinaire","tu sauras","toutes ces choses","le bonheur en face","a la plus haute branche","a vous","ma force","trois heures vingt"),("Loved Me Back to Life","Somebody Loves Somebody","Incredible","Water and a Flame","Breakaway","Save Your Soul","Didn't Know Love","Thank You","Overjoyed","Thankful","At Seventeen","Always Be Your Girl","Unfinished Songs","How Do You Keep the Music Playing","Lullaby"),("Parler à mon père","Le miracle","Qui peut vivre sans amour?","L'amour peut prendre froid","Attendre","Une chance qu'on s'a","La mer et l'enfant","Moi quand je pleure","Celle qui m'a tout appris","Je n'ai pas besoin d'amour","Si je n'ai rien de toi","Que toi au monde","Tant de Temps","Les petits pieds de Léa","Ne me quitte pas","Les jours comme ça"),("Taking Chances","Alone","Eyes On Me","My Love","Shadow Of Love","Surprise Surprise","This Time","New Dawn","A Song For You","A World To Believe In","Can't Fight The Feelin'","I Got Nothin' Left","Right Next To The Right One","Fade Away","That's Just The Woman In Me","Skies Of L.A.","Map to My Heart","The Reason I Go On")
+
+    # ------------------maitre gims----------------------
+
+    gims_lefleau,gims_ceinturenoire,gims_acontrecoeur=("intro","immortel","côté noir","oats","pendejo","origami","yolo","oro jackson","twenny twenny","og na og","sicario","jusqu'ici tout va bien","jetez pas l'oeil","grosse bleta","dans ma tete","c'est comme ca","thomas shelby"),("Intro","Tant pis","Caméléon","Fuegolando","La même","Loup Garou","Entre nous c'est mort","Laissez-moi tranquille","Mi Gna","Tu reviendras","Nos valeurs","Tu ne le vois pas","Merci maman","T'es partie","Tu m'as dit","Oulala","Bonita","Le pire","Où aller","Tu m'as mis dans la merde","Anakin","Malheur, Malheur","Mon cœur meurtri","Gunshot","La nuit c'est fait pour dormir","Tu me l'avais promis","Corazon","Ana fi dar","Skyfall","Appelez la police","Caméléon (feat. Quincy)","PS","Chien de la casse","Je t'en veux","Everytime","La vérité","Les roses ont des épines","60%","Je n'ai rien promis"),("Ma beauté (Pilule violette)","150 (Pilule violette)","Loin (Pilule violette)","Boucan (Pilule violette)","Tout donner (Pilule violette)","Paname (Pilule violette)","Pense à moi (Pilule violette)")
+
+    # ---------------------------album_youssoupha---------------------
+
+    youssoupha_ennoiretblanc,youssoupha_noirdesire,youssoupha_ngrtd,youssoupha_polaroidexperience=("La Foule","Clashes","Poids plume","Revolver","La foule, Pt. 2","Yoka","Rap franc CFA","Haut-parleur","Droopy","Rien a changé","Apprentissage","Rap franc CFA","Revolver","Poids plume","Haut-parleur"),("L'amour","Viens","J'ai changé","Menace de mort","Histoires vraies","Irréversible","Les Disques de mon père","L'Enfer c'est les autres","B.A.O. (Bouche à oreille)","Gestelude, pt. 1 (feat. Sam's)","Noir désir","Tout l'amour du monde","Dreamin'","Gestelude, pt. 2 (feat. S-pi)","La vie est belle","(feat. Lfdv)","Espérance de vie","4h37 (Outro)"),("Où est l'amour ?","Salaam","Points communs","Maman m'a dit","Memento","Love Musik","À cause de moi","Chanson française","Entourage","Le score","Niquer ma vie","Mannschaft","Black Out","Smile","Negritude","Mourir mille fois"),("Polaroid experience","La Cassette","Devenir vieux","M'en aller","Avoir de l'argent","Les Sentiments à l'envers","Alléluia / 1989","Par amour","Niama na yo","Devant","Mourir ensemble","Le jour où j'ai arrêté le rap")
+
+    # ---------------------------album_fally_ipupa-----------------
+
+    fally_tokoos2,fally_control,fally_tokoos=("Milolo","Obomanga","Likolo (feat. Ninho)","Nairobi","Juste une fois","8ème merveille","Amore","Oza yanga (feat. Naza)","Santé","Si tu me laisses t’aimer","Un coup","Mon bébé","Couleurs","Madany","Migrant des rêves","Animation"),("Control","Canne à sucre","Aime-moi","Code d'amour","Maria PM","Punition","La guerre","Photo","Humanisme","Sans amour","Mbongwana","The Crown","Bafana","Jus d'orange","A Flyé","S. Mushigo","Bombe","Roi Manitou","Ndaye Late Dior","DG Landry","Vouloir sans pouvoir","207","École","Sans pitié","Soucis","One Love","Bafana Love","Doc Jeff","C'est bon","World You The Best"),("Kiname (feat. Booba)","Esengo","Na Lingui Yé (feat. MHD","Yakuza (feat. Wizkid)","Mannequin (feat. Keblack & Naza)","Tout le monde danse","Guerrier (feat. Shay)","Siamois","Posa","Ça va aller","Jeudi soir","Bad Boy (feat. Aya Nakamura)","Boulé","Belle-fille","Champ","Nidja (feat. R. Kelly)","Juste une danse","Eloko Oyo")
+
+# ----------------------album_christ_brown-----------------------
+
+    chris_graffiti,chris_inmyzone,chris_fortune,chris_X=("I Can Transform Ya","Sing Like Me","Crawl","So Cold","What I Do","Famous Girl","Take My Time","I.Y.A.","Pass Out","Wait","Lucky Me","Fallin Down","I'll Go","Girlfriend","They Say"),("Turnt Up","Too Freaky","Convertible","Don't Lie","Bad","Invented Head","Shoes","Big Booty Judy","Madusa","Back Out","Work Wit It","Say Ahh","I Get Around","No Bullshit","Twitter","How Low Can You Go","T.Y.A.","I Wanna Rock","Perfume","Sex","Glow in the Dark"),("Turn Up the Music","Bassline","Till I Die","Mirage","Don't Judge Me","2012","Biggest Fan","Sweet Love","Strip","Stuck On Stupid","4 Years Old","Party Hard / Cadillac (Interlude)","Don't Wake Me Up","Trumpet Lights"),("X","Add Me In","Loyal","New Flame","Songs On 12 Play","101 (Interlude)","Drown In It","Came To Do","Stereotype","Time For Love","Lady In A Glass Dress (Interlude)","Autumn Leaves","Do Better","See You Around","Don't Be Gone Too Long","Body Shots","Drunk Texting","Lost In Ya Love","Love More","Don't Think They Know","Fine China")
+
+class Artists:
+
+    fally={"tokoos2":Albums.fally_tokoos2,"control":Albums.fally_control,"tokooos":Albums.fally_tokoos}
+
+    ed_sheeran={"equals":Albums.ed_sheeran_equals,"collaboration":Albums.ed_sheeran_collaboration,"divid":Albums.ed_sheeran_divid,"x":Albums.ed_sheeran_x}
+
+    celine_dion={"courage":Albums.celine_dion_courage,"encore un soir":Albums.celine_dion_encoreunsoir,"love me back to life":Albums.celine_dion_lovemebacktolife,"sans attendre":Albums.celine_dion_sansattendre,"taking chances":Albums.celine_dion_takingchances}
+
+    gims={"le fleau":Albums.gims_lefleau,"ceinture noire":Albums.gims_ceinturenoire,"a contre coeur":Albums.gims_acontrecoeur}
+
+    youssoupha={"en noir et blanc":Albums.youssoupha_ennoiretblanc,"noire desire":Albums.youssoupha_noirdesire,"ngrtd":Albums.youssoupha_ngrtd,"polaroide d'experience":Albums.youssoupha_polaroidexperience}
+
+    chris={"graffiti":Albums.chris_graffiti,"in my zone":Albums.chris_inmyzone,"fortune":Albums.chris_fortune,"x":Albums.chris_X}
+
+
+    
+    artists={"fally":fally,"ed_sheeran":ed_sheeran,"gims":gims,"youssoupha":youssoupha,"celine dion":celine_dion,"chris brown":chris}
+
+
+
+def album(artist):
+    print(f"voici tous les albums de {artist}")
+    print("")
+    print(("\n").join(list(Artists.artists[artist].keys())))
+
+
+def title(artist,artist_album):
+    print(f"voici tous les titres de {artist_album}")
+    print("")
+    print(("\n").join(list(Artists.artists[artist][artist_album])))
+
+
+
+
---
+++ artists.py
@@ -0,0 +1,14 @@
+import albumsartists as al
+
+artist=input("entrer le nom de votre artiste préféré:")
+
+al.album(artist)
+
+
+artist_album=input("entrer votre album préféré pour voir ces titres:")
+
+al.title(artist,artist_album)
+
+
+
+    
